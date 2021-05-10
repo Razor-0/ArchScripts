06415b823585bf91db62405c49d3f5021e97883e
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2021-05-10 14:06:26 +0200

    Auto LuKs setup
    be sure to edit the scrip and replace PASSWORD in the "printf lines" at the beginning to use your own key for the drives

--- Partitions.sh
+++ Partitions.sh
@@ -7,11 +7,11 @@ pvcreate /dev/sda3
 vgcreate vgroot /dev/sda3
 lvcreate -l 100%FREE -n btrfs vgroot
 
-printf "123" | cryptsetup -q luksFormat --type luks1 --use-urandom -h sha1 -i 1000 /dev/sda2
-printf "123" | cryptsetup open /dev/sda2 esp
+printf "PASSWORD" | cryptsetup -q luksFormat --type luks1 --use-urandom -h sha1 -i 1000 /dev/sda2
+printf "PASSWORD" | cryptsetup luksOpen /dev/sda2 esp
 
-printf "123" | cryptsetup -q luksFormat --type luks2 --use-urandom -h sha512 -i 1000 /dev/vgroot/btrfs
-printf "123" | cryptsetup open /dev/vgroot/btrfs root
+printf "PASSWORD" | cryptsetup -q luksFormat --type luks2 --use-urandom -h sha512 -i 1000 /dev/vgroot/btrfs
+printf "PASSWORD" | cryptsetup luksOpen /dev/vgroot/btrfs root
 
 mkfs.vfat -F12 /dev/sda1
 mkfs.vfat -F32 /dev/mapper/esp
