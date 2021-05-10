0707416520c49c765e908b031804212c393541d2
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2021-05-10 13:53:10 +0200

    Update Partitions.sh

--- Partitions.sh
+++ Partitions.sh
@@ -7,15 +7,15 @@ pvcreate /dev/sda3
 vgcreate vgroot /dev/sda3
 lvcreate -l 100%FREE -n btrfs vgroot
 
-cryptsetup luksFormat --type luks1 --use-urandom -h sha1 -i 1000 /dev/sda2
-printf "123" | cryptsetup luksFormat --type luks1 --use-urandom -h sha1 -i 1000 /dev/sda2
-cryptsetup open /dev/sda2 esp
-printf "123" | cryptsetup open /dev/sda2 esp
+cryptsetup -q luksFormat --type luks1 --use-urandom -h sha1 -i 1000 /dev/sda2
+printf "123" | cryptsetup -q luksFormat --type luks1 --use-urandom -h sha1 -i 1000 /dev/sda2
+cryptsetup -q open /dev/sda2 esp
+printf "123" | cryptsetup -q open /dev/sda2 esp
 
-cryptsetup luksFormat --type luks2 --use-urandom -h sha512 -i 1000 /dev/vgroot/btrfs
-printf "123" | cryptsetup luksFormat --type luks2 --use-urandom -h sha512 -i 1000 /dev/vgroot/btrfs
-cryptsetup open /dev/vgroot/btrfs root
-printf "123" | cryptsetup open /dev/vgroot/btrfs
+cryptsetup -q luksFormat --type luks2 --use-urandom -h sha512 -i 1000 /dev/vgroot/btrfs
+printf "123" | cryptsetup -q luksFormat --type luks2 --use-urandom -h sha512 -i 1000 /dev/vgroot/btrfs
+cryptsetup -q open /dev/vgroot/btrfs root
+printf "123" | cryptsetup -q open /dev/vgroot/btrfs
 
 mkfs.vfat -F12 /dev/sda1
 mkfs.vfat -F32 /dev/mapper/esp
