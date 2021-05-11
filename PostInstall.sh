978b7f9354c613805d264f50e099d2f0b524bbe0
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2021-05-12 00:01:33 +0200

    add yay, some pacman stuff and tg

--- PostInstall.sh
+++ PostInstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
 
+# setup snapper for rollback snapshots
 sudo umount /.snapshots
 sudo umount /home/.snapshots
 sudo rm -r /.snapshots
@@ -19,16 +20,42 @@ sudo systemctl enable --now snapper-timeline.timer
 sudo systemctl enable --now snapper-cleanup.timer
 sudo systemctl enable --now snapper-boot.timer
 
+# install some neccessities and plasma DE
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm xorg xf86-video-intel xf86-input-synaptics nvidia nvidia-dkms plasma plasma-pa sddm pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire pulseeffects pavucontrol konsole kate chromium dolphin dolphin-plugins packagekit-qt5 zsh zsh-autosuggestions zsh-syntax-highlighting openssh
+sudo pacman -S --noconfirm xorg xf86-video-intel xf86-input-synaptics nvidia nvidia-dkms plasma plasma-pa sddm pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire pulseeffects pavucontrol konsole kate chromium dolphin dolphin-plugins packagekit-qt5 zsh zsh-autosuggestions zsh-syntax-highlighting openssh python python2 python3 python-pip speedtest-cli qemu qemu-arch-extra virt-manager android-tools vlc obs-studio code gtkmm fuse2 pcsclite libcanberra
 
 sudo systemctl enable sddm
 sudo systemctl enable sshd
 
+# install yay aur helper, font for pwrlvl10k and snap-pac-grub
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm PKGBUILD
 cd
 
 yay -S --answerclean all --noconfirm ttf-meslo-nerd-font-powerlevel10k snap-pac-grub
+
+# install ohmyzsh for user
+wget https://github.com/ChrisTitusTech/zsh/raw/master/.zshrc -O ~/.zshrc
+mkdir -p "$HOME/.zsh"
+wget https://github.com/ChrisTitusTech/zsh/raw/master/aliasrc -O ~/.zsh/aliasrc
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# ohmyzsh pwrlvl10k theme and plugins
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# install updateable telegram desktop
+tgver=2.6.1
+
+mkdir $HOME/Downloads/install/Telegram
+cd $HOME/Downloads/install/Telegram
+wget https://github.com/telegramdesktop/tdesktop/releases/download/v${tgver}/tsetup.${tgver}.tar.xz
+tar -xJvf tsetup.${tgver}.tar.xz
+sudo mv Telegram /opt/telegram
+sudo ln -sf /opt/telegram/Telegram /usr/bin/telegram
+cd $HOME
+
 neofetch
