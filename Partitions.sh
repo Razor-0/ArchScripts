be19ddb149b913a677ae3f3d824645e2db53bfb8
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2021-05-12 22:54:04 +0200

    removed tmp subvol for tmpfs

--- Partitions.sh
+++ Partitions.sh
@@ -34,7 +34,6 @@ btrfs su cr /mnt/@/root
 btrfs su cr /mnt/@/opt
 btrfs su cr /mnt/@/srv
 btrfs su cr /mnt/@/swap
-btrfs su cr /mnt/@/tmp
 btrfs su cr /mnt/@/snapshots
 btrfs su cr /mnt/@/snapshots/home
 btrfs su cr /mnt/@/snapshots/root
@@ -58,33 +57,32 @@ btrfs su cr /mnt/@/usr/local
 umount /mnt
 
 # mounting the subvolumes and partititons
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@ /dev/mapper/root /mnt
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@ /dev/mapper/root /mnt
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/home /dev/mapper/root /mnt/home
 mkdir -p /mnt/{boot,.snapshots,.win}
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/home /dev/mapper/root /mnt/home
 mkdir /mnt/home/.snapshots
 mkdir -p /mnt/.win/{ssd,hdd,ehdd,usb,iso}
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/root /dev/mapper/root /mnt/root
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/opt /dev/mapper/root /mnt/opt
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/srv /dev/mapper/root /mnt/srv
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,swap,subvol=@/swap /dev/mapper/root /mnt/swap
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/tmp /dev/mapper/root /mnt/tmp
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/cache /dev/mapper/root /mnt/var/cache
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/crash /dev/mapper/root /mnt/var/crash
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/log /dev/mapper/root /mnt/var/log
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/opt /dev/mapper/root /mnt/var/opt
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/spool /dev/mapper/root /mnt/var/spool
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/tmp /dev/mapper/root /mnt/var/tmp
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/lib/libvirt/images /dev/mapper/root /mnt/var/lib/libvirt/images
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/lib/machines /dev/mapper/root /mnt/var/lib/machines
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/lib/portables /dev/mapper/root /mnt/var/lib/portables
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/lib/mailman /dev/mapper/root /mnt/var/lib/mailman
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/lib/named /dev/mapper/root /mnt/var/lib/named
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/lib/mariadb /dev/mapper/root /mnt/var/lib/mariadb
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/lib/mysql /dev/mapper/root /mnt/var/lib/mysql
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/var/lib/pgqsl /dev/mapper/root /mnt/var/lib/pgqsl
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/usr/local /dev/mapper/root /mnt/usr/local
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/snapshots/home /dev/mapper/root /mnt/home/.snapshots
-mount -o defaults,discard,noatime,compress=zstd:3,space_cache=v2,subvol=@/snapshots/root /dev/mapper/root /mnt/.snapshots
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/root /dev/mapper/root /mnt/root
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/opt /dev/mapper/root /mnt/opt
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/srv /dev/mapper/root /mnt/srv
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,swap,subvol=@/swap /dev/mapper/root /mnt/swap
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/cache /dev/mapper/root /mnt/var/cache
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/crash /dev/mapper/root /mnt/var/crash
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/log /dev/mapper/root /mnt/var/log
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/opt /dev/mapper/root /mnt/var/opt
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/spool /dev/mapper/root /mnt/var/spool
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/tmp /dev/mapper/root /mnt/var/tmp
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/lib/libvirt/images /dev/mapper/root /mnt/var/lib/libvirt/images
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/lib/machines /dev/mapper/root /mnt/var/lib/machines
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/lib/portables /dev/mapper/root /mnt/var/lib/portables
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/lib/mailman /dev/mapper/root /mnt/var/lib/mailman
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/lib/named /dev/mapper/root /mnt/var/lib/named
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/lib/mariadb /dev/mapper/root /mnt/var/lib/mariadb
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/lib/mysql /dev/mapper/root /mnt/var/lib/mysql
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/var/lib/pgqsl /dev/mapper/root /mnt/var/lib/pgqsl
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/usr/local /dev/mapper/root /mnt/usr/local
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/snapshots/home /dev/mapper/root /mnt/home/.snapshots
+mount -o defaults,discard,noatime,compress=zstd:1,space_cache=v2,subvol=@/snapshots/root /dev/mapper/root /mnt/.snapshots
 mount /dev/mapper/esp /mnt/boot
 mkdir /mnt/boot/efi
 mount /dev/sde1 /mnt/boot/efi
@@ -103,7 +101,7 @@ chattr +C /mnt/swap/swapfile
 dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=24576 status=progress # edit count value to change swap's size
 chmod 600 /mnt/swap/swapfile
 mkswap /mnt/swap/swapfile
-swapon -p 4 /mnt/swap/swapfile # edit -p value to set different priority per requirements
+swapon -p 40 /mnt/swap/swapfile # edit -p value to set different priority per requirements
 
 # installing base system and some neccessities
 pacstrap /mnt base linux-lts linux-firmware nano intel-ucode reflector git sh curl wget
