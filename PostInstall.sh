d1b8ff16b976614a072878d5335342b5bb2f9ada
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2021-05-16 15:33:04 +0200

    Update PostInstall.sh

--- PostInstall.sh
+++ PostInstall.sh
@@ -53,4 +53,18 @@ cd
 sudo cp /etc/snap-pac/root.conf{.example,}
 sudo cp /etc/snap-pac/root.conf /etc/snap-pac/home.conf
 
+# clone ohmyzsh and config files
+wget https://github.com/ChrisTitusTech/zsh/raw/master/.zshrc -O ~/.zshrc
+mkdir -p "$HOME/.zsh"
+wget https://github.com/ChrisTitusTech/zsh/raw/master/aliasrc -O ~/.zsh/aliasrc
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
+# install ohmyzsh and switch to zsh (exit zsh to continue the scripts)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install powerlevel10k and zsh plugins
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 neofetch
