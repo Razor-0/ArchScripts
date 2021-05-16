de2da9e0c94ee7eb3b1376b367b95f690ba7cddc
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2021-05-16 15:34:04 +0200

    Update PostInstall.sh

--- PostInstall.sh
+++ PostInstall.sh
@@ -67,4 +67,7 @@ git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerl
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# install cowsay and lolcat (mainly to update latest snapshot)
+sudo pacman -S lolcat cowsay
+
 neofetch
