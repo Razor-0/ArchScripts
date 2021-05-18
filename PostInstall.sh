94ecabb06bb360b547004e8420057a32ece08df7
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2021-05-18 13:12:35 -0700

    integrating post into chroot

--- PostInstall.sh
+++ PostInstall.sh
@@ -21,14 +21,6 @@ sudo systemctl enable --now snapper-timeline.timer
 sudo systemctl enable --now snapper-cleanup.timer
 sudo systemctl enable --now snapper-boot.timer
 
-# install some neccessities and plasma DE
-sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm xorg xf86-video-intel xf86-input-synaptics nvidia-lts nvidia-prime nvidia-settings plasma plasma-pa sddm pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire pulseeffects pavucontrol konsole kate chromium dolphin dolphin-plugins packagekit-qt5 zsh zsh-autosuggestions zsh-syntax-highlighting openssh
-
-# enable display manager and ssh
-sudo systemctl enable sddm
-sudo systemctl enable sshd
-
 # install yay aur helper, font for pwrlvl10k and snap-pac-grub
 mkdir $HOME/Downloads/install
 cd $HOME/Downloads/install
