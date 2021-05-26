25a68b847b92844a68fc181a291da3a0d921e589
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2021-05-26 08:22:03 +0200

    update post install

--- PostInstall.sh
+++ PostInstall.sh
@@ -23,7 +23,7 @@ sudo systemctl enable --now snapper-boot.timer
 
 # install desktop environment
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm xorg xf86-video-intel xf86-input-synaptics nvidia-lts nvidia-prime nvidia-settings nvidia-dkms plasma plasma-pa sddm pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire pulseeffects pavucontrol konsole kate chromium dolphin dolphin-plugins packagekit-qt5 openssh micro xclip
+sudo pacman -S --noconfirm xorg xf86-video-intel xf86-input-synaptics nvidia-lts nvidia-prime nvidia-settings nvidia-dkms plasma plasma-pa sddm pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire pulseeffects pavucontrol konsole kate chromium dolphin dolphin-plugins packagekit-qt5 openssh micro xclip dialog screen tree
 
 # enable display manager and ssh
 systemctl enable sddm
