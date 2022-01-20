95fe817b2a28c1391a857919a0249290717fee36
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2022-01-20 17:34:06 +0100

    Update PostInstall.sh

--- PostInstall.sh
+++ PostInstall.sh
@@ -4,51 +4,23 @@ set -eu
 # update if necessary and setup snapper for snapshot rollback
 sudo pacman -Syyu --noconfirm
 sudo umount /.snapshots
-sudo umount /home/.snapshots
 sudo rm -r /.snapshots
-sudo rm -r /home/.snapshots
 sudo snapper -c root create-config /
-sudo snapper -c home create-config /home
 sudo btrfs su de /.snapshots
-sudo btrfs su de /home/.snapshots
 sudo mkdir /.snapshots
-sudo mkdir /home/.snapshots
 sudo mount -a
 sudo chmod 750 /.snapshots
-sudo chmod 750 /home/.snapshots
 
 # enable timeline and cleanup (please edit snapper configs to your specific needs)
 sudo systemctl enable --now snapper-timeline.timer
 sudo systemctl enable --now snapper-cleanup.timer
 sudo systemctl enable --now snapper-boot.timer
 
-# install yay aur helper, font for pwrlvl10k and snap-pac-grub
-mkdir $HOME/Downloads/install
-cd $HOME/Downloads/install
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm PKGBUILD
-yay -S --answerclean all --noconfirm ttf-meslo-nerd-font-powerlevel10k snap-pac-grub
-
 # install desktop environment and some stuff
-sudo pacman -S --noconfirm xorg xorg-server xf86-video-intel xf86-input-synaptics nvidia-lts nvidia-prime nvidia-settings plasma plasma-pa sddm konsole yakuake dolphin dolphin-plugins pipewire pipewire-pulse pipewire-alsa pipewire-jack gst-plugin-pipewire easyeffects pavucontrol kate chromium keepassxc libreoffice-fresh libreoffice-fresh-en-gb libreoffice-fresh-hu libreoffice-fresh-de packagekit-qt5 qbittorrent openssh micro xclip dialog screen tree wget curl sh neofetch zsh zsh-syntax-highlighting zsh-autosuggestions
+sudo pacman -S --noconfirm xorg-server xf86-video-intel xf86-input-synaptics plasma plasma-pa sddm konsole dolphin dolphin-plugins pipewire pipewire-pulse pipewire-alsa pipewire-jack kate chromium keepassxc packagekit-qt5 qbittorrent openssh micro xclip screen tree neofetch zsh zsh-syntax-highlighting zsh-autosuggestions
 
 # enable display manager and ssh
 sudo systemctl enable sddm
 sudo systemctl enable sshd
 
-# install updateable telegram desktop
-tgver=2.6.1
-mkdir $HOME/Downloads/install/Telegram
-cd $HOME/Downloads/install/Telegram
-wget https://github.com/telegramdesktop/tdesktop/releases/download/v${tgver}/tsetup.${tgver}.tar.xz
-tar -xJvf tsetup.${tgver}.tar.xz
-sudo mv Telegram /opt/telegram
-sudo ln -sf /opt/telegram/Telegram /usr/bin/telegram
-
-# clone ohmyzsh config files
-wget https://github.com/ChrisTitusTech/zsh/raw/master/.zshrc -O ~/.zshrc
-mkdir -p "$HOME/.zsh"
-wget https://github.com/ChrisTitusTech/zsh/raw/master/aliasrc -O ~/.zsh/aliasrc
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 neofetch
