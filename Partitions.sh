87747029a3c7da6fb0c123cc2357659ac538792c
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2022-01-26 19:44:47 +0100

    Update Partitions.sh

--- Partitions.sh
+++ Partitions.sh
@@ -52,7 +52,7 @@ echo '<snapshot>' >> /mnt/@/snapshots/root/1/info.xml
 echo '  <type>single</type>' >> /mnt/@/snapshots/root/1/info.xml
 echo '  <num>1</num>' >> /mnt/@/snapshots/root/1/info.xml
 DATE="$(date +"%Y-%m-%d %H:%M:%S")"
-echo '$DATE' | sed -i "5s//<date>$DATE<\/date>" /mnt/@/snapshots/root/1/info.xml
+echo '$DATE' | sed -i "5s//<date>$DATE<\/date>/" /mnt/@/snapshots/root/1/info.xml
 echo '  <description>Original Root Filesystem</description>' >> /mnt/@/snapshots/root/1/info.xml
 echo '</snapshot>' >> /mnt/@/snapshots/root/1/info.xml
 btrfs su set-default $(btrfs su li /mnt | grep "@/snapshots/root/1/snapshot" | grep -oP'(?<=ID)[0-9]+') /mnt
