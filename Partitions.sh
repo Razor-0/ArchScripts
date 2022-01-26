a23f34075f814eb3fcc3037b1c50ca4e865b4326
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2022-01-26 14:46:11 +0100

    Update Partitions.sh

--- Partitions.sh
+++ Partitions.sh
@@ -4,7 +4,7 @@ set -eu
 # create LuKs boot and LuKs root
 # change the PASSWORD in all 4 of these lines (first to create then to open said LuKs part)
 echo "PASSWORD" | cryptsetup -q luksFormat --type luks1 --use-urandom -h sha1 -i 1000 /dev/sda2
-echo "PASSWORD" | cryptsetup luksOpen /dev/sda2 esp
+echo "PASSWORD" | cryptsetup luksOpen /dev/sda2 boot
 
 echo "PASSWORD" | cryptsetup -q luksFormat --type luks2 --use-urandom -h sha512 -i 1000 /dev/sda3
 echo "PASSWORD" | cryptsetup luksOpen /dev/sda3 root
@@ -12,12 +12,11 @@ echo "PASSWORD" | cryptsetup luksOpen /dev/sda3 root
 # formatting partitions with the following filesystems
 mkfs.vfat -F32 /dev/sda1
 fatlabel /dev/sda1 Bootloaders
-mkfs.vfat -F32 /dev/mapper/esp
-fatlabel /dev/mapper/esp Kernels
-mkfs.btrfs -L 'Btrfs Root' /dev/mapper/vgroot-btrfs
+mkfs.reiserfs -l Kernels /dev/mapper/boot
+mkfs.btrfs -L 'Btrfs Root' /dev/mapper/root
 
 # creating btrfs subvols for snapshots
-mount /dev/mapper/vgroot-btrfs /mnt
+mount /dev/mapper/root /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@/home
 btrfs su cr /mnt/@/root
@@ -45,30 +44,30 @@ btrfs su cr /mnt/@/usr/local
 umount /mnt
 
 # mounting the subvolumes and partititons
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@ /dev/mapper/vgroot-btrfs /mnt
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/home /dev/mapper/vgroot-btrfs /mnt/home
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@ /dev/mapper/root /mnt
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/home /dev/mapper/root /mnt/home
 mkdir -p /mnt/{boot,.win}
 mkdir -p /mnt/.win/{ssd,hdd,ehdd,usb,iso}
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/root /dev/mapper/vgroot-btrfs /mnt/root
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/opt /dev/mapper/vgroot-btrfs /mnt/opt
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/srv /dev/mapper/vgroot-btrfs /mnt/srv
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,swap,subvol=@/.swap /dev/mapper/vgroot-btrfs /mnt/.swap
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/cache /dev/mapper/vgroot-btrfs /mnt/var/cache
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/crash /dev/mapper/vgroot-btrfs /mnt/var/crash
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/log /dev/mapper/vgroot-btrfs /mnt/var/log
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/opt /dev/mapper/vgroot-btrfs /mnt/var/opt
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/spool /dev/mapper/vgroot-btrfs /mnt/var/spool
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/tmp /dev/mapper/vgroot-btrfs /mnt/var/tmp
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/libvirt/images /dev/mapper/vgroot-btrfs /mnt/var/lib/libvirt/images
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/machines /dev/mapper/vgroot-btrfs /mnt/var/lib/machines
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/portables /dev/mapper/vgroot-btrfs /mnt/var/lib/portables
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/mailman /dev/mapper/vgroot-btrfs /mnt/var/lib/mailman
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/named /dev/mapper/vgroot-btrfs /mnt/var/lib/named
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/mariadb /dev/mapper/vgroot-btrfs /mnt/var/lib/mariadb
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/mysql /dev/mapper/vgroot-btrfs /mnt/var/lib/mysql
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/pgqsl /dev/mapper/vgroot-btrfs /mnt/var/lib/pgqsl
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/usr/local /dev/mapper/vgroot-btrfs /mnt/usr/local
-mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/.snapshots /dev/mapper/vgroot-btrfs /mnt/.snapshots
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/root /dev/mapper/root /mnt/root
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/opt /dev/mapper/root /mnt/opt
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/srv /dev/mapper/root /mnt/srv
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,swap,subvol=@/.swap /dev/mapper/root /mnt/.swap
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/cache /dev/mapper/root /mnt/var/cache
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/crash /dev/mapper/root /mnt/var/crash
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/log /dev/mapper/root /mnt/var/log
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/opt /dev/mapper/root /mnt/var/opt
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/spool /dev/mapper/root /mnt/var/spool
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/tmp /dev/mapper/root /mnt/var/tmp
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/libvirt/images /dev/mapper/root /mnt/var/lib/libvirt/images
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/machines /dev/mapper/root /mnt/var/lib/machines
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/portables /dev/mapper/root /mnt/var/lib/portables
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/mailman /dev/mapper/root /mnt/var/lib/mailman
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/named /dev/mapper/root /mnt/var/lib/named
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/mariadb /dev/mapper/root /mnt/var/lib/mariadb
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/mysql /dev/mapper/root /mnt/var/lib/mysql
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/var/lib/pgqsl /dev/mapper/root /mnt/var/lib/pgqsl
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/usr/local /dev/mapper/root /mnt/usr/local
+mount -o defaults,autodefrag,discard,noatime,compress=zstd:5,space_cache=v2,subvol=@/.snapshots /dev/mapper/root /mnt/.snapshots
 mount /dev/mapper/esp /mnt/boot
 mkdir /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
