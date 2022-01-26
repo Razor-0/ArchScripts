05ef866ee3824646e3cf6ba5abcf57f0127be057
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2022-01-26 15:21:28 +0100

    Update Arch-chroot.sh

--- Arch-chroot.sh
+++ Arch-chroot.sh
@@ -11,7 +11,7 @@ sed -i '93s/#//' /etc/pacman.conf # comment this and the next command to not ena
 sed -i '94s/#//' /etc/pacman.conf
 
 # edit this command to use the correct mirrorlist for your country
-reflector --latest 4 --protocol https --sort rate --verbose --save /etc/pacman.d/mirrorlist
+reflector --country Netherlands --latest 6 --protocol https --sort rate --verbose --save /etc/pacman.d/mirrorlist
 
 echo 'lenarch' >> /etc/hostname # edit lenarch to whatever name you want for your PC
 echo '127.0.0.1	localhost' >> /etc/hosts
@@ -26,7 +26,7 @@ echo razor:PASSWORD | chpasswd # same here for the user's PASSWORD
 
 # edit as you see fit alongside the systemctl commands
 pacman -Syyu --noconfirm
-pacman -S --noconfirm grub efibootmgr os-prober btrfs-progs ntfs-3g dosfstools linux-zen-headers base-devel xdg-user-dirs alsa-utils xdg-utils networkmanager network-manager-applet wpa_supplicant bluez bluez-utils tlp acpi acpi_call-dkms acpid nfs-utils lvm2 linux-lts linux-lts-headers rsync snapper doas
+pacman -S --noconfirm grub efibootmgr os-prober btrfs-progs ntfs-3g dosfstools linux-zen-headers base-devel xdg-user-dirs alsa-utils xdg-utils networkmanager wpa_supplicant bluez bluez-utils tlp acpi acpi_call-dkms acpid nfs-utils lvm2 linux linux-headers rsync snapper doas git
 
 # enable neccessities like Network, BT etc at boot
 systemctl enable NetworkManager
@@ -50,25 +50,25 @@ echo 'KERNEL=="zram1", ATTR{disksize}="2048M" RUN="/usr/bin/mkswap /dev/zram1", 
 echo 'KERNEL=="zram2", ATTR{disksize}="2048M" RUN="/usr/bin/mkswap /dev/zram2", TAG+="systemd"' >> /etc/udev/rules.d/99-zram.rules
 echo 'KERNEL=="zram3", ATTR{disksize}="2048M" RUN="/usr/bin/mkswap /dev/zram3", TAG+="systemd"' >> /etc/udev/rules.d/99-zram.rules
 
-# create hook to encrypt esp at boot
-cp /usr/lib/initcpio/install/encrypt /etc/initcpio/install/encryptesp
-cp /usr/lib/initcpio/hooks/encrypt /etc/initcpio/hooks/encryptesp
-sed -i 's/cryptdevice/cryptesp/' /etc/initcpio/hooks/encryptesp
-sed -i 's/cryptkey/cryptespkey/' /etc/initcpio/hooks/encryptesp
+# create hook to decrypt esp at boot
+cp /usr/lib/initcpio/install/encrypt /etc/initcpio/install/encryptboot
+cp /usr/lib/initcpio/hooks/encrypt /etc/initcpio/hooks/encryptboot
+sed -i 's/cryptdevice/cryptboot/' /etc/initcpio/hooks/encryptboot
+sed -i 's/cryptkey/cryptbootkey/' /etc/initcpio/hooks/encryptboot
 
 # create keys to unlock esp and root at boot
 mkdir /root/.keys
 chmod 700 /root/.keys
-head -c 64 /dev/urandom >> /root/.keys/espkey.bin
+head -c 64 /dev/urandom >> /root/.keys/bootkey.bin
 head -c 64 /dev/urandom >> /root/.keys/rootkey.bin
-chmod 600 /root/.keys/espkey.bin
+chmod 600 /root/.keys/bootkey.bin
 chmod 600 /root/.keys/rootkey.bin
-echo "PASSWORD" | cryptsetup -v luksAddKey -i 1 /dev/sda2 /root/.keys/espkey.bin # edit PASSWORD
+echo "PASSWORD" | cryptsetup -v luksAddKey -i 1 /dev/sda2 /root/.keys/bootkey.bin # edit PASSWORD
 echo "PASSWORD" | cryptsetup -v luksAddKey -i 1 /dev/sda3 /root/.keys/rootkey.bin # same here
 
 # set temp environment value to include in grub config
-ESP="$(blkid -s UUID -o value /dev/sda2)"
-BTRFS="$(blkid -s UUID -o value /dev/sda3)"
+BOOT="$(blkid -s UUID -o value /dev/sda2)"
+ROOT="$(blkid -s UUID -o value /dev/sda3)"
 
 # edit grub config and grubd to make btrfs decide the default subvolume
 sed -i '66,78 {s/^/#/}' /etc/grub.d/10_linux
@@ -76,7 +76,7 @@ sed -i '4s/5/8/' /etc/default/grub
 sed -i '13s/#//' /etc/default/grub
 sed -i '54s/#//' /etc/default/grub
 sed -i '/above./a GRUB_DEFAULT=saved' /etc/default/grub
-echo '$ESP','$BTRFS' | sed -i "6s/.*/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 cryptesp=UUID=$ESP:esp cryptespkey=rootfs:\/root\/.keys\/espkey.bin cryptdevice=UUID=$BTRFS:root cryptkey=rootfs:\/root\/.keys\/rootkey.bin root=\/dev\/mapper\/vgroot-btrfs rw resume=\/dev\/mapper\/vgroot-btrfs resume_offset=16400\"/" /etc/default/grub
+echo '$ESP','$BTRFS' | sed -i "6s/.*/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 cryptboot=UUID=$BOOT:boot cryptbootkey=rootfs:\/root\/.keys\/bootkey.bin cryptdevice=UUID=$ROOT:root cryptkey=rootfs:\/root\/.keys\/rootkey.bin root=\/dev\/mapper\/root rw resume=\/dev\/mapper\/root resume_offset=16400\"/" /etc/default/grub
 
 # add sudo and doas privileges to the user
 echo 'permit persist razor as root' >> /etc/doas.conf # change razor to your user or uncomment this and remove doas from the pacman part if you don't need doas
@@ -84,17 +84,17 @@ echo 'razor ALL=(ALL) ALL' | EDITOR=tee visudo /etc/sudoers.d/rootusers # change
 
 # edit fstab for btrfs and add zram to automount
 sed -i 's/,subvolid=256,subvol=\/@//' /etc/fstab
-echo '/dev/zram0		none		swap		defaults,pri=400	0 0' >> /etc/fstab
+echo '/dev/zram0		none		swap		defaults,pri=32000	0 0' >> /etc/fstab
 echo >> /etc/fstab
-echo '/dev/zram1		none		swap		defaults,pri=400	0 0' >> /etc/fstab
+echo '/dev/zram1		none		swap		defaults,pri=16000	0 0' >> /etc/fstab
 echo >> /etc/fstab
-echo '/dev/zram2		none		swap		defaults,pri=400	0 0' >> /etc/fstab
+echo '/dev/zram2		none		swap		defaults,pri=8000	0 0' >> /etc/fstab
 echo >> /etc/fstab
-echo '/dev/zram3		none		swap		defaults,pri=400	0 0' >> /etc/fstab
+echo '/dev/zram3		none		swap		defaults,pri=4000	0 0' >> /etc/fstab
 
 # set default btrfs subvolume for snapper and install grub, gen init and grub config
 btrfs su set-default 256 /
 mkinitcpio -p linux-zen
-mkinitcpio -p linux-lts
+mkinitcpio -p linux
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Arch Linux x64"
 grub-mkconfig -o /boot/grub/grub.cfg
