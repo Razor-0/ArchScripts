be3a4bbb47a1600fc76b16de96bbe38c7a38a31e
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2022-02-06 00:09:20 +0100

    Update Post-Install.sh

--- Post-Install.sh
+++ Post-Install.sh
@@ -3,7 +3,7 @@ set -eu
 
 # refreshing reflector and installing kde
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm snapper xorg-server xf86-video-intel xf86-input-synaptics plasma sddm konsole dolphin pipewire pipewire-pulse pipewire-alsa kate chromium intel-ucode
+sudo pacman -S --noconfirm snapper xorg-server xf86-video-intel xf86-input-synaptics plasma sddm konsole dolphin pipewire pipewire-pulse pipewire-alsa kate chromium intel-ucode nano
 sudo systemctl enable sddm
 
 # creating snappers config
