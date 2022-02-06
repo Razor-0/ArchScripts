6d7db8f510bf9f43089690633afe63c8c6b45469
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2022-02-06 17:30:57 +0100

    Update Arch-chroot.sh

--- Arch-chroot.sh
+++ Arch-chroot.sh
@@ -55,7 +55,7 @@ sed -i '13s/#//' /etc/default/grub
 sed -i '54s/#//' /etc/default/grub
 sed -i '64s/#//' /etc/default/grub
 sed -i '/above./a GRUB_DEFAULT=saved' /etc/default/grub
-echo '$BOOT' | sed -i "/none/a boot     UUID=$BOOT      /root/.keys/bootkey.bin" /etc/crypttab
+echo '$BOOT' | sed -i "/none/a boot	UUID=$BOOT	/root/.keys/bootkey.bin" /etc/crypttab
 echo '$ROOT' | sed -i "6s/.*/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 cryptdevice=UUID=$ROOT:root cryptkey=rootfs:\/root\/.keys\/rootkey.bin root=\/dev\/mapper\/root rw resume=\/dev\/mapper\/root resume_offset=16400\"/" /etc/default/grub
 
 # enable 2GB zram pages per physical core on 4C/8T
