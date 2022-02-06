cf6b9a44d5b7a5950670b7d3b0392bcde71f1ec6
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2022-02-06 02:44:00 +0100

    Update Partitions.sh

--- Partitions.sh
+++ Partitions.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # change the PASSWORD in all 4 of these lines (first to create then to open said LuKs part)
-echo "PASSWORD" | cryptsetup -q luksFormat --type luks2 --use-urandom -h sha512 -i 1000 /dev/sda2
+echo "PASSWORD" | cryptsetup -q luksFormat --type luks1 --use-urandom -h sha256 -i 1000 /dev/sda2
 echo "PASSWORD" | cryptsetup luksOpen /dev/sda2 root
 
 # formatting partitions with the following filesystems
