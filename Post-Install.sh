b98d17d790c1bf38c46e651247281c4c1015374e
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2022-02-12 22:01:59 +0100

    add packages

--- Post-Install.sh
+++ Post-Install.sh
@@ -3,7 +3,7 @@ set -eu
 
 # refreshing reflector and installing kde
 sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm snapper xorg-server xf86-video-intel xf86-input-synaptics plasma sddm konsole dolphin pipewire pipewire-pulse pipewire-alsa kate chromium
+sudo pacman -S --noconfirm snapper xorg-server xf86-video-intel xf86-input-synaptics plasma sddm konsole dolphin pipewire pipewire-pulse pipewire-alsa kate chromium git curl wget sh
 
 # creating snappers config
 sudo umount /.snapshots
@@ -13,3 +13,9 @@ sudo btrfs su de /.snapshots
 sudo mkdir /.snapshots
 sudo mount -a
 sudo chmod 750 /.snapshots
+
+# install paru pacman wrapper for AUR packages
+mkdir ~/Downloads/tmpAur
+git clone https://aur.archlinux.org/paru.git ~/Downloads/tmpAur
+cd ~/Downloads/tmpAur/paru
+makepkg -sic PKGBUILD
