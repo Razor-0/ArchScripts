c8ad5c5daf879d178414eca638e8bea3493d8abb
Razor-0 <60897851+Razor-0@users.noreply.github.com>
2022-02-13 19:09:43 +0100

    Update Post-Install.sh

--- Post-Install.sh
+++ Post-Install.sh
@@ -5,14 +5,20 @@ set -eu
 sudo pacman -Syyu --noconfirm
 sudo pacman -S --noconfirm snapper xorg-server xf86-video-intel xf86-input-synaptics plasma sddm konsole dolphin pipewire pipewire-pulse pipewire-alsa kate chromium git curl wget sh neofetch ttf-opensans micro xclip code zsh-autosuggestions zsh-syntax-highlighting
 
-# creating snappers config
+# creating snapper configs
 sudo umount /.snapshots
+sudo umount /home/.snapshots
 sudo rm -r /.snapshots
+sudo rm -r /home/.snapshots
 sudo snapper -c root create-config /
+sudo snapper -c home create-config /home
 sudo btrfs su de /.snapshots
+sudo btrfs su de /home/.snapshots
 sudo mkdir /.snapshots
+sudo mkdir /home/.snapshots
 sudo mount -a
 sudo chmod 750 /.snapshots
+sudo chmod 750 /home/.snapshots
 
 # install paru pacman wrapper for AUR packages
 git clone https://aur.archlinux.org/paru.git ~/Downloads/tmpParu
